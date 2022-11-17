c39f176b113e7ab834d29ae406a102d3265343d6
gedizz <gedizzletsplay@gmail.com>
2022-11-17 20:31:25 +0000

    create program structure

---
+++ grocery-list-manager.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ingredient {
+    char name[50];
+    int amount;
+    char measureType[20];
+
+}ingredient;
+
+typedef struct recipe {
+    char name[100];
+    ingredient list[50];
+
+
+}recipe;
+
+void readFile(recipe list[], int numRecipes);
+//void addRecipe()
+//void printRecipes()
+//void removeRecipe()
+//void addIngredient()
+//void printGroceryList()
+
+
+int main(int *argc, int argv[]) {
+
+
+
+    return 0;
+}
+
+
+void readFile(recipe list[], int numRecipes) {
+
+    continue;
+}
